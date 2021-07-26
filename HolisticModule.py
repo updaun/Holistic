b1afb1d963e6a40124bb4042caf6f0ea400915bc
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-07-26 17:54:52 +0900

    turtleneck project draft

--- HolisticModule.py
+++ HolisticModule.py
@@ -17,6 +17,8 @@ class HolisticDetector():
         self.min_tracking_confidence = min_tracking_confidence
 
         self.mpHolistic = mp.solutions.holistic
+        self.mpPose = mp.solutions.pose
+        self.mpFace = mp.solutions.face_mesh
         self.holistics = self.mpHolistic.Holistic(self.static_image_mode, self.model_complexity, self.smooth_landmarks, self.min_detection_confidence, self.min_tracking_confidence)
         self.mpDraw = mp.solutions.drawing_utils
 
@@ -26,100 +28,84 @@ class HolisticDetector():
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB )
         self.results = self.holistics.process(imgRGB)
 
-        if self.results.pose_landmarks.landmark:
-            for holisticLms in self.results.pose_landmarks.landmark:
-                if draw :
-                    self.mpDraw.draw_landmarks(img, holisticLms, self.mpHolistic.POSE_CONNECTIONS)
+        if self.results.pose_landmarks:
+            
+            if draw:
+                 # Draw pose, left and right hands, and face landmarks on the image.
+                annotated_image = img.copy()
+
+                self.mpDraw.draw_landmarks(
+                    annotated_image, self.results.face_landmarks, self.mpHolistic.FACE_CONNECTIONS)
+                self.mpDraw.draw_landmarks(
+                    annotated_image, self.results.left_hand_landmarks, self.mpHolistic.HAND_CONNECTIONS)
+                self.mpDraw.draw_landmarks(
+                    annotated_image, self.results.right_hand_landmarks, self.mpHolistic.HAND_CONNECTIONS)
+                self.mpDraw.draw_landmarks(
+                    annotated_image, self.results.pose_landmarks, self.mpHolistic.POSE_CONNECTIONS)
+
+                # Plot pose world landmarks.
+                # self.mpDraw.plot_landmarks(
+                #     self.results.pose_world_landmarks, self.mpHolistic.POSE_CONNECTIONS)
+                return annotated_image
+            
         return img
 
-#     def findPosition(self, img, handNo=0, draw=True):
-#         xList = []
-#         yList = []
-#         bbox = []
-#         self.lmList = []
-#         if self.results.multi_hand_landmarks:
-#             myHand = self.results.multi_hand_landmarks[handNo]
+    def findPoseLandmark(self, img, draw=True):
+        xList = []
+        yList = []
 
-#             for id, lm in enumerate(myHand.landmark):
-#                 #print(id,lm)
-#                 h, w, c = img.shape
-#                 cx, cy = int(lm.x*w), int(lm.y*h)
-#                 #print(id, cx, cy)
-#                 xList.append(cx)
-#                 yList.append(cy)
-#                 self.lmList.append([id, cx, cy])
-#                 #if id == 4:
-#                 if draw:
-#                     cv2.circle(img, (cx,cy), 7, (255,0,0),cv2.FILLED) 
+        self.pose_lmList = []
+        if self.results.pose_landmarks:
+            myHolistic = self.results.pose_landmarks
+            # print(myHolistic.landmark)
+            # print(type(myHolistic.landmark))
+            for id, lm in enumerate(myHolistic.landmark):
+                # print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                # print(id, cx, cy)
+                xList.append(cx)
+                yList.append(cy)
+                self.pose_lmList.append([id, cx, cy])
 
-#             xmin, xmax = min(xList), max(xList)
-#             ymin, ymax = min(yList), max(yList)
-#             bbox = xmin, ymin, xmax, ymax
+        return self.pose_lmList
 
-#             if draw:
-#                 cv2.rectangle(img, (xmin-20, ymin-20), (xmax + 20, ymax + 20),
-#                 (0,255,0), 2)
+    def findFaceLandmark(self, img, draw=True):
+        xList = []
+        yList = []
 
-#         return self.lmList, bbox
+        self.face_lmList = []
+        if self.results.face_landmarks:
+            myHolistic = self.results.face_landmarks
+            # print(myHolistic.landmark)
+            # print(type(myHolistic.landmark))
+            for id, lm in enumerate(myHolistic.landmark):
+                # print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                # print(id, cx, cy)
+                xList.append(cx)
+                yList.append(cy)
+                self.face_lmList.append([id, cx, cy])
 
-#     def fingersUp(self):
-#         fingers = []
-#         # Thumb(Doesn't filp)
-#         # if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
-#         # Thumb(Filp)
-#         if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
-#             fingers.append(1)
-#         else:
-#             fingers.append(0)
-
-#         # Fingers except Thumb
-#         for id in range(1, 5):
-#             if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id]-2][2]:
-#                 fingers.append(1)
-#             else:
-#                 fingers.append(0)
-
-#         return fingers
-
-#     def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
-#         x1, y1 = self.lmList[p1][1:]
-#         x2, y2 = self.lmList[p2][1:]
-#         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-
-#         if draw:
-#             cv2.line(img, (x1, y1), (x2, y2), (255,0,255), t)
-#             cv2.circle(img, (x1, y1), r, (255,0,255), cv2.FILLED)
-#             cv2.circle(img, (x2, y2), r, (255,0,255), cv2.FILLED)
-#             cv2.circle(img, (cx, cy), r, (0,0,255), cv2.FILLED)
-#         length = math.hypot(x2-x1, y2-y1)
-
-#         return length, img, [x1, y1, x2, y2, cx, cy]
-
-# def main():
-#     pTime = 0
-#     cTime = 0
-#     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#     detector = handDetector()
-#     while True:
-#         success, img = cap.read()
-#         img = detector.findHands(img)
-#         lmList = detector.findPosition(img)
-#         if len(lmList) != 0:
-#             print(lmList[4])
+        return self.face_lmList
 
 
-#         cTime = time.time()
-#         fps = 1/(cTime-pTime)
-#         pTime = cTime
+    def findCenter(self, p1, p2):
+        x1, y1 = self.pose_lmList[p1][1:]
+        x2, y2 = self.pose_lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-#         cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+        return cx, cy
 
-#         cv2.imshow("Image", img)
-#         if cv2.waitKey(1) & 0xFF == 27:
-#             break 
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.face_lmList[p1][1:]
+        x2, y2 = p2[0],p2[1]
 
-#     cap.release()
-#     cv2.destroyAllWindows()
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255,0,255), t)
+            cv2.circle(img, (x1, y1), r, (255,0,255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255,0,255), cv2.FILLED)
+        length = math.hypot(x2-x1, y2-y1)
 
-# if __name__ == "__main__":
-#     main()
+        return length, img
--- Holistic_classbased.py
+++ Holistic_classbased.py
@@ -8,12 +8,32 @@ cTime = 0
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 detector = hm.HolisticDetector()
 
+turtle_neck_count = 0
+
 while True:
     success, img = cap.read()
 
-    img = detector.findHolistic(img, draw=False)
+    img = detector.findHolistic(img, draw=True)
 
-   
+    pose_lmList = detector.findPoseLandmark(img, draw=True)
+    face_lmList = detector.findFaceLandmark(img, draw=True)
+
+    if len(pose_lmList) != 0 and len(face_lmList) != 0:
+        # print("pose[11]", pose_lmList[11])
+        # print("pose[12]", pose_lmList[12])
+        # print("face[152]",face_lmList[152])
+
+        center_shoulder = detector.findCenter(11,12)
+        length, img = detector.findDistance(152, center_shoulder, img, draw=True)
+        # print(length)
+        if length < 100:
+            turtle_neck_count += 1
+
+        if length < 100 and turtle_neck_count > 100:
+            print("WARNING - Keep your posture straight.")
+            print("TurtleNeck Point = ", int(length))
+            turtle_neck_count = 0
+
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
@@ -21,6 +41,7 @@ while True:
     cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
 
     cv2.imshow("Image", img)
+
     if cv2.waitKey(1) & 0xFF == 27:
         break 
 
