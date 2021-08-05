64ff79f0dc36d9507678852c12c7055d34cb4453
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-08-06 01:06:21 +0900

    module path change

--- Holistic_turtleneck_eyeblink.py
+++ Holistic_turtleneck_eyeblink.py
@@ -1,5 +1,5 @@
 import cv2
-import HolisticModule as hm
+import modules.HolisticModule as hm
 from win10toast import ToastNotifier
 import math
 import time
--- Holistic_turtleneck_noimgview.py
+++ Holistic_turtleneck_noimgview.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
-import HolisticModule as hm
+import modules.HolisticModule as hm
 from win10toast import ToastNotifier
 import math
 
